77c218e057dc1298224490ccc0dc250537222ccd
Alan Montoya <alanmontoya2010@att.net>
2019-03-22 22:19:44 -0500

    more javascript I hope

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,10 +26,37 @@ var guessesSofar=0;
 // This function is run whenever the user presses a key.
 
 document.onkeyup=function(event) {
+
     player=event.key;
+
     // randomly chooses a letter for "what letter am i thinking of"
     computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+if ((player==="a") || (player==="e") || (player="i")) {
+
+    if (player==="a" && computerGuess==="") ||
+        (player==="e" && computerGuess==="") ||
+        (player==="i" && computerGuess==="") || {
+            ++wins;
+    }
+
+
+
+
 }
+
+
+        // Display the user and computer guesses, and wins/losses/ties.\
+        userChoiceText.textContent = "You chose: " + player;
+        computerChoiceText.textContent = "The computer chose: " + computerGuess;
+        winsText.textContent ="Wins:" + wins;
+        lossesText.textContent = "Loses: " + loses;
+        leftText.textContent = "Guesses Left:" + guessesLeft;
+        sofarText.textContent = "Guesses So Far: " + guessesSofar;
+      }
+
+
+
 
 
 // creating the  variables that hold references to the places in the html file where displayed
@@ -37,6 +64,8 @@ var nameText = document.getElementById("container");
 var userChoiceText = document.getElementById("userchoice");
 var computerChoiceText = document.getElementById("computerchoice");
 var winsText = document.getElementById("wins");
-var lossesText = document.getElementById("losses");
+var lossesText = document.getElementById("loses");
 var leftText = document.getElementById("guesses-left");
 var sofarText= document.getElementById("guesses-so-far");
+
+
